d31414f6475a43ffef149d5dd48996b6c73b54a7
khang <khang194591@gmail.com>
2025-01-11 19:56:14 +0700

    update

--- forms/schemas/list-form.contract.ts
+++ forms/schemas/list-form.contract.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
+import { PaginationResponseSchema } from '~/contracts/common';
 
-export const ListFormResponseSchema = z.object({
+export const ListFormResponseSchema = PaginationResponseSchema.extend({
   items: z.array(
     z.object({
       id: z.coerce.number(),
@@ -9,6 +10,12 @@ export const ListFormResponseSchema = z.object({
       isTemplate: z.boolean(),
       responseEditable: z.boolean(),
       multipleResponse: z.boolean(),
+    }),
+  ),
+  formTemplates: z.array(
+    z.object({
+      id: z.coerce.number(),
+      name: z.string(),
     }),
   ),
 });
